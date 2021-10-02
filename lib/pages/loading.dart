5697ba60e60524c422b7bf2984546c942f24cc04
Hritambh <hritambh9@gmail.com>
2021-10-02 11:19:38 +0530

    day 25...adding http dependencies

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
-class Loading extends StatelessWidget {
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void getData() async {
+
+    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+    // print(response.body);
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,4 +32,3 @@ class Loading extends StatelessWidget {
     );
   }
 }
-
