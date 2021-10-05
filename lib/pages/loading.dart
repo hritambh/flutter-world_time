e335e2930f5189d68568f741b375cde368819042
Hritambh <hritambh9@gmail.com>
2021-10-05 09:22:17 +0530

    day33_images of day night

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class Loading extends StatelessWidget {
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Asia/Kolkata');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+        backgroundColor: Colors.blue[900],
+        body: Center(
+            child: SpinKitFadingCube(
+              color: Colors.white,
+              size: 50.0,
+            )
+        )
     );
   }
 }
-
