b1d345b1c5bd48f41b93b95f44fe644fd479c90e
Hritambh <hritambh9@gmail.com>
2021-10-07 23:49:55 +0530

    offset1 & offset 2

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -10,6 +10,7 @@ class WorldTime {
   String url; // location url for api endpoint
   bool isDaytime; // true or false if daytime or not
 
+
   WorldTime({ this.location, this.flag, this.url });
 
   Future<void> getTime() async {
@@ -21,11 +22,14 @@ class WorldTime {
 
       // get properties from json
       String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(1,3);
+      String offset1 = data['utc_offset'].substring(1,3);
+      String offset2 = data['utc_offset'].substring(4,6);
 
       // create DateTime object
       DateTime now = DateTime.parse(datetime);
-      now = now.add(Duration(hours: int.parse(offset)));
+      now = now.add(Duration(hours: int.parse(offset1), minutes: int.parse(offset2)));
+      print(now);
+
 
       // set the time property
       isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
